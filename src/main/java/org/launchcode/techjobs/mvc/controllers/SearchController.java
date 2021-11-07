78a1bdcf6cae1e84bc00d13b0aaf1885af308884
Melanie Gaddis <mellow1984@charter.net>
2021-11-07 12:19:14 -0600

    all tests pass.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -27,7 +27,7 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @PostMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
         model.addAttribute("columns", columnChoices);
 
